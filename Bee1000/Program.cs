4273f292a023f6e363b6e995985f0f067daa8eac
AbiMa <AbiMa@ABIMA-PC>
2022-12-16 19:00:34 -0300

    Adicionar arquivos de projeto.

---
+++ Bee1000/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bee1000
+{
+    class Bee1000
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
